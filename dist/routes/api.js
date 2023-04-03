dbabf36c59f1c7af15a56e88069337ebd68cbf56
Piya Seesamer <piya.ssm@gmail.com>
2023-04-04 00:03:53 +0700

    remove

--- dist/routes/api.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-var issueController = require("../controllers/issueController")["default"];
-module.exports = function (app) {
-  app.route("/api/issues/:project").get(issueController.viewIssues).post(issueController.createIssue).put(issueController.editIssue)["delete"](issueController.deleteIssue);
-};
